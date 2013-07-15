30bc9680432a200e9d28f150e54ec8dfa1c176e5
Kamil Madac <kamil.madac@gmail.com>
2013-07-15 10:47:42 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='NetappSDK5.0',
+    version='1.0',
+    packages=['NetappSDK'],
+    url='',
+    license='',
+    author='kmadac',
+    author_email='kamil.madac@t-systems.sk',
+    description='Netapp SDK 5.0 package'
+)
